d9870144f54c037cac7ec44425699c15d6761b60
IvanonlAndr <96746263+IvanonlAndr@users.noreply.github.com>
2022-12-01 13:27:53 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.icon').on('click', function () {
+    $('.header__menu').toggleClass('header__menu-close');
+  });
+  $('.icon').on('click', function () {
+    $('.header__menu-items').toggleClass('header__menu-items-active');
+  });
